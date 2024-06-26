7f24491befe7656964f5149e3f9e8ccf5a05a66a
Waheed Abbas <abbaxwaheed@gmail.com>
2024-06-26 17:17:09 +0500

    Integrated Facebook SDK and deferred deep links.

--- app/src/main/java/com/example/facebook_deep_links_android/MainActivity.kt
+++ app/src/main/java/com/example/facebook_deep_links_android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.facebook_deep_links_android
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,11 +10,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.facebook_deep_links_android.ui.theme.FacebookdeeplinksandroidTheme
+import com.facebook.applinks.AppLinkData
+import com.facebook.applinks.AppLinkData.createFromActivity
 
 class MainActivity : ComponentActivity() {
+
+    private var greetingText by mutableStateOf("Android")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,11 +29,27 @@ class MainActivity : ComponentActivity() {
             FacebookdeeplinksandroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = greetingText,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
+        }
+
+        // Check for deep links when activity is created
+        checkDeepLinks()
+    }
+
+    private fun checkDeepLinks() {
+        // Check if the app was opened from a deep link
+        val appLinkData: AppLinkData? = createFromActivity(this)
+
+        if (appLinkData != null && appLinkData.targetUri != null) {
+            // Handle deep link URL, extract parameters, and navigate to relevant content
+            greetingText = "Android - launched via web deep links"
+        } else {
+            // Handle the case where the app was opened without a deep link
+            greetingText = "Android"
         }
     }
 }
